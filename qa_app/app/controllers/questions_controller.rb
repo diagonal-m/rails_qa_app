e7e61901dbb8db7328ad7daf10b88d76a4dc57c2
佐藤太一 <3101taichi@gmail.com>
2021-07-01 10:36:07 +0900

    ログインユーザーの操作を限定的に

--- qa_app/app/controllers/questions_controller.rb
+++ qa_app/app/controllers/questions_controller.rb
@@ -22,9 +22,9 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    @question = Question.new(question_params)
+    @question = current_user.questions.new(question_params)
     if @question.save
-      redirect_to @questions, notice: "「#{@question.title}を投稿しました」"
+      redirect_to questions_path, notice: "「#{@question.title}を投稿しました」"
     else
       render :new
     end
--- qa_app/db/schema.rb
+++ qa_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_26_212310) do
+ActiveRecord::Schema.define(version: 2021_07_01_005451) do
 
   create_table "questions", force: :cascade do |t|
     t.string "title", null: false
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2021_06_26_212310) do
     t.boolean "solved", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
