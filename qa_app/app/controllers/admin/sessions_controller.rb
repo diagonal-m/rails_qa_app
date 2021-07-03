e322813cfbe573d26771b81b7de7ecf4e0487a15
佐藤太一 <3101taichi@gmail.com>
2021-07-03 17:57:38 +0900

    管理者ログイン画面作成

---
+++ qa_app/app/controllers/admin/sessions_controller.rb
@@ -0,0 +1,22 @@
+class Admin::SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by(email: session_params[:email])
+
+    return render(:new) unless user.admin
+
+    if user&.authenticate(session_params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: 'adminユーザーとしてログインしました'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
+end
--- qa_app/config/routes.rb
+++ qa_app/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get '/login', to: 'sessions#new'
+    post '/login', to: 'sessions#create'
+  end
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
